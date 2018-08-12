e2ff7528cc5d4c17136bb524dceb1c15463fcbde
Renzo R. Carpio Ponce <me@renzocarpio.com>
2018-08-12 09:16:07 -0500

    se agrego loader para css uno para entender y otro para cargar css

---
+++ css-style-loader/index.js
@@ -0,0 +1,2 @@
+import './estilos.css'
+console.log('Hola mundo!, desde WEBPACK en un webpack.config.js 2');
---
+++ css-style-loader/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path'); 
+module.exports = {
+    entry: path.resolve(__dirname,'index.js'),
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                //test: que tipo de archivo quiero reconocer,
+                //use: que loader se va a encargar del archivo
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    }
+}
